f95b1b17fd1a8902efec76e09178de445e758a88
SmiFod <samif306@gmail.com>
2025-03-20 13:04:33 +0100

    ye

--- app.py
+++ app.py
@@ -1,27 +1,74 @@
-from flask import Flask, request, redirect, session
+from flask import Flask, render_template, redirect, url_for, request, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
-app.secret_key = 'hemmelig'
-users = {}
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'hemmelignøkkel'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login"
 
-@app.route('/', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        if users.get(request.form['username']) == request.form['password']:
-            session['user'] = request.form['username']
-            return redirect('/welcome')
-    return '<form method="post">Brukernavn: <input name="username"> Passord: <input name="password" type="password"> <button type="submit">Login</button></form>'
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
 
-@app.route('/register', methods=['GET', 'POST'])
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    if request.method == 'POST':
-        users[request.form['username']] = request.form['password']
-        return redirect('/')
-    return '<form method="post">Brukernavn: <input name="username"> Passord: <input name="password" type="password"> <button type="submit">Registrer</button></form>'
+    if request.method == "POST":
+        username = request.form["username"]
+        password = bcrypt.generate_password_hash(request.form["password"]).decode("utf-8")
+        if User.query.filter_by(username=username).first():
+            flash("Brukernavn er allerede tatt!", "danger")
+            return redirect(url_for("register"))
+        new_user = User(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        flash("Registrering vellykket! Logg inn nå.", "success")
+        return redirect(url_for("login"))
+    return render_template("register.html")
 
-@app.route('/welcome')
-def welcome():
-    return f'Velkommen {session.get("user", "Gjest")}! <a href="/">Logg ut</a>'
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        user = User.query.filter_by(username=username).first()
+        if user and bcrypt.check_password_hash(user.password, password):
+            login_user(user)
+            flash("Innlogging vellykket!", "success")
+            return redirect(url_for("quiz"))
+        flash("Feil brukernavn eller passord", "danger")
+    return render_template("login.html")
 
-if __name__ == '__main__':
+@app.route("/quiz")
+@login_required
+def quiz():
+    questions = [
+        {"question": "Hva er hovedstaden i Norge?", "options": ["Oslo", "Bergen", "Trondheim"], "answer": "Oslo"},
+        {"question": "Hvor mange kontinenter finnes det?", "options": ["5", "6", "7"], "answer": "7"},
+    ]
+    return render_template("quiz.html", questions=questions)
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash("Du er logget ut.", "info")
+    return redirect(url_for("home"))
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
